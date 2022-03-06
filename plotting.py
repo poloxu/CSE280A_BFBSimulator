73e613235acb22abf3d4c1fa2907d8b2b3a77c97
lena_krocken <lenakrocken@gmail.com>
2022-03-06 01:01:55 -0800

    Add files via upload
    Plotting File - just histogram currently

---
+++ plotting.py
@@ -0,0 +1,32 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+## plot count vector as a histogram
+## given: count vector and n number of cycles
+def plotCountsHist(count_array, n):
+    labels = [i for i in range(1, len(count_array[0]))]
+    X = np.arange(len(labels))
+    
+    length = len(count_array[0])
+    y1_index = 0
+    y2_index = (len(count_array)//2)-1
+    y3_index = (4*len(count_array)//5)-1
+    y4_index = len(count_array)-1
+    
+    w = 0.5/3
+    ax = plt.subplot(111)
+    ax.bar(X-2*w, count_array[y1_index][1:length], width = w, label = "1 cycle")
+    ax.bar(X-w,count_array[y2_index][1:length], width = w, label = str(y2_index+1) + " cycles", color = "red")
+    ax.bar(X+w, count_array[y3_index][1:length], width = w, label = str(y3_index+1) + " cycles", color = "purple")
+    ax.bar(X+2*w, count_array[y4_index][1:length], width = w, label = str(y4_index+1) + " cycles", color = "green")
+    
+    ax.set_ylabel('Frequencies')
+    ax.set_title('Segment Counts after ' + str(n) + ' BFB cycles')
+    ax.set_xticks(X)
+    ax.set_xticklabels(labels)
+    ax.set_xticks(X, labels)
+    ax.set_xlabel("Segments")
+    ax.legend()
+    
+    plt.show()
