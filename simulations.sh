285be74335a1ad47d3ddafd357a2102f0f0621e6
Lena Krockenberger <lkroken@notifier@genomemaster.ucsd.edu>
2022-08-10 13:35:24 -0700

    comments added

--- simulations.sh
+++ simulations.sh
@@ -20,16 +20,17 @@ for i in ${!cancerGenes[@]}; do
 	echo chromosome $1 and position $2 and gene $3
 	
 	## Simple BFB simulations - no indels and no end deletions
-	#endPos=$(( $2 + 3000000 ))
+	endPos=$(( $2 + 3000000 ))
 	#echo end position $endPos
-	#echo COMMAND:./BFBSimulator.py 5 $1 $endPos new_hg38.fa -df 0 -dl 0 -dd simple -xf 0 -xl 0 -xd simple -o outputs/simple/${3} -p 0 -b 0 -bl 0
-	#./BFBSimulator.py 5 $1 $2 $endPos new_hg38.fa -df 0 -dl 0 -dd simple -xf 0 -xl 0 -xd simple -o outputs/simple/${3} -p 0 -b 0 -bl 0
+	./BFBSimulator.py 5 $1 $2 $endPos new_hg38.fa -df 0 -dl 0 -dd simple -xf 0 -xl 0 -xd simple -o outputs/simple/${3} -p 0 -b 0 -bl 0
 	
+	## End deletion simulations - end deletions but no indels
 	for index in 1 2 3; do
 		endPos=$(( $2 + 6000000 ))
 		./BFBSimulator.py 10 $1 $2 $endPos new_hg38.fa -df 0 -dl 0 -dd simple -xf 0 -xl 0 -xd simple -o outputs/end_Deletions/${3}_${index} -p 0 -b 1 -bl 50000	
 	done
 
+	## Complex deletion simulations - indels and end deletions (defualt parameters)
 	for ind in 1 2; do
                 endPos=$(( $2 + 6000000 ))
                 ./BFBSimulator.py 10 $1 $2 $endPos new_hg38.fa -o outputs/complex/${3}_${ind}
